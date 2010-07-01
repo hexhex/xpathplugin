211be74c01badd51703ed894002b6febbd914d1d
Thomas Krennwallner <tkren@kr.tuwien.ac.at>
2010-07-01 14:07:46 +0000

    Updating to version currently used in dlvhex trunk.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,46 @@
 #!/bin/sh
+#
+# Thomas Krennwallner <tkren@kr.tuwien.ac.at>
+#
+# 1. check for libtool, autoconf, automake, pkg-config
+# 2. run libtoolize and autoreconf to create libltdl, configure, and Makefile.in's
+#
 
-echo "libtoolize..."
-libtoolize || glibtoolize \
-    || { echo "libtoolize failed"; exit 1; }
+LT=`which libtoolize`
+GLT=`which glibtoolize`
 
-echo "aclocal..."
-# use this if you have additional files in the m4 directory:
-aclocal -I m4 \
-    || { echo "aclocal failed"; exit 1; }
-echo "autoheader..."
-autoheader --warnings=all \
-    || { echo "autoheader failed"; exit 1; }
-echo "automake..."
-automake --warnings=all --warnings=no-syntax --foreign --add-missing \
-    || { echo "automake failed"; exit 1; }
+if [ -x "$LT" ]; then
+    LT=libtoolize
+elif [ -x "$GLT" ]; then
+    LT=glibtoolize
+else
+    echo "libtoolize: command not found. Please install GNU libtool, GNU autoconf, and GNU automake."
+    exit 1
+fi
 
-echo "autoconf..."
-autoconf --warnings=all \
-    || { echo "autoconf failed"; exit 1; }
+ARC=`which autoreconf`
+
+if [ ! -x "$ARC" ]; then
+    echo "autoreconf: command not found. Please install GNU autoconf and GNU automake."
+    exit 1
+fi
+
+AM=`which automake`
+
+if [ ! -x "$AM" ]; then
+    echo "automake: command not found. Please install GNU automake."
+    exit 1
+fi
+
+PC=`which pkg-config`
+
+if [ ! -x "$PC" ]; then
+    echo "pkg-config: command not found. Please install pkg-config."
+    exit 1
+fi
+
+# copy libltdl into working repository
+$LT -c -f --ltdl
+
+# rebuild autotool files
+autoreconf -f -i -W all
