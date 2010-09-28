655173209f10f645ea783d28ceedff873f5973e7
Thomas Krennwallner <tkren@kr.tuwien.ac.at>
2010-09-28 03:28:00 +0000

    Setup Boost.Test environment.

--- testsuite/xpath-unittest.cpp
+++ testsuite/xpath-unittest.cpp
@@ -1,4 +1,37 @@
-int main (int argc, char* arg[])
+#include "xpathatom.hpp"
+#include "xpathplugin.hpp"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "xpath-unittest"
+#include <boost/test/unit_test.hpp>
+
+#include <fstream>
+#include <iostream>
+
+using namespace dlvhex;
+using namespace dlvhex::xpath;
+
+BOOST_AUTO_TEST_CASE( XPathAtomTest )
 {
-	return 0;
+  const char* ex = getenv("EXAMPLESDIR");
+  assert (ex != 0);
+
+  XPathAtom* xa = new XPathAtom;
+
+  BOOST_CHECK(xa != 0);
+
+  delete xa;
+}
+
+
+BOOST_AUTO_TEST_CASE( XPathPluginTest )
+{
+  const char* ex = getenv("EXAMPLESDIR");
+  assert (ex != 0);
+
+  XPathPlugin* xp = new XPathPlugin;
+
+  BOOST_CHECK(xp != 0);
+
+  delete xp;
 }
